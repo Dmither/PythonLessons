1884084de296926fefe42ade23a3799c3bcc1787
Dmither <mr.d.graumann@gmail.com>
2023-02-24 16:38:41 +0200

    update

---
+++ main.py
@@ -0,0 +1,28 @@
+def write_table(lst):
+    file = open("./data/index.txt", "w")
+    for i in range(len(lst)):
+        file.writelines([str(lst[i][0]), "\t", "|", "\t"])
+        file.writelines([str(lst[i][1]), "\t", "|", "\t"])
+        file.writelines([str(lst[i][2]), "\n"])
+    file.close()
+
+
+def read_table():
+    lst = []
+    file = open("./data/index.txt", "r")
+    for i in file:
+        item = i[:-1].split('\t|\t')
+        lst.append(item)
+    file.close()
+    return lst
+
+
+lst = [
+    ["bread", 13.8, 12],
+    ["chocolate", 28.5, 5],
+    ["potato", 8, 5.5],
+]
+
+
+# ---------------------------------------------------------------
+
