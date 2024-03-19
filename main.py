4c9ad7f7062a7331459698b9e5f043316414c1d6
Dmither <mr.d.graumann@gmail.com>
2024-03-19 14:23:06 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-file1 = open("./text.txt", "a")
-file1.write("\nAdd some more text")
-file1.close()
+# my_list = list((1, 2, 3))
+# iterator = iter(my_list)
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
 
-file1 = open("./text.txt", "r")
-print(file1.read())
-file1.close()
+class PowTwo:
+  def __init__(self, max=0):
+    self.max = max
+  def __iter__(self):
+    self.n = 0
+    return self
+  def __next__(self):
+    if self.n <= self.max:
+      result = 2 ** self.n
+      self.n += 1
+      return result
+    else:
+      raise StopIteration
+    
+numbers = PowTwo(3)
+
+for num in numbers:
+  print(num)
+
+# class MyList(list):
+#   def __next__(self):
+#     print("Hello")
+#     super().__next__(self)
+
+# my_list_2 = MyList((1, 2, 3))
+# iterator2 = iter(my_list_2)
+# print(next(iterator2))
+# print(next(iterator2))
+# print(next(iterator2))
